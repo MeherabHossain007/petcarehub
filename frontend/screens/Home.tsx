f7e10be3293d043a900df6ce598e0861c0021125
Yousufjoy <kanekiken333@gmail.com>
2023-11-24 19:05:00 +0600

    home page half part done

--- frontend/screens/Home.tsx
+++ frontend/screens/Home.tsx
@@ -2,8 +2,12 @@ import { Image, View, StyleSheet, TouchableOpacity, Text } from "react-native";
 import React from "react";
 import { Heading, Icon, Input, Stack, VStack } from "native-base";
 import { AntDesign } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import cat1 from "../assets/Cat1.png";
+import cat2 from "../../../petcarehub/frontend/assets/Cat2.png";
 
 export default function Home() {
+  const bottom_tab = createBottomTabNavigator();
   return (
     <>
       <View style={style.inputContainer}>
@@ -27,6 +31,21 @@ export default function Home() {
           />
         </VStack>
       </View>
+      <View style={style.heroMain}>
+        <View style={style.heroLeft}>
+          <Text style={style.mainText}>Shop Now!</Text>
+          <Text style={style.secondText}>
+            Shop from our website and get exclusive products!
+          </Text>
+          <TouchableOpacity style={style.btnStyle}>
+            <Text style={style.btnText}>Get Started</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={style.heroRight}>
+          <Image source={cat1} />
+          <Image style={style.imgBottom} source={cat2} />
+        </View>
+      </View>
     </>
   );
 }
@@ -35,5 +54,51 @@ const style = StyleSheet.create({
   inputContainer: {
     marginTop: 50,
     marginLeft: 30,
+  },
+  btnStyle: {
+    backgroundColor: "#2196F3",
+    padding: 10,
+    borderRadius: 5,
+    width: 100,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  btnText: {
+    color: "white",
+  },
+  heroMain: {
+    backgroundColor: "#EEE9F2",
+    flexDirection: "row",
+    marginTop: 30,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingTop: 20,
+    paddingLeft: 20,
+  },
+  heroLeft: {
+    width: 200,
+    // borderWidth: 2,
+    // borderColor: "black",
+    // borderRadius: 10,
+  },
+  heroRight: {
+    width: 150,
+    position: "relative",
+    // borderWidth: 2,
+    // borderColor: "black",
+    // borderRadius: 10,
+  },
+  imgBottom: {
+    position: "absolute",
+    bottom: -30,
+    left: -60,
+  },
+  mainText: {
+    fontSize: 22,
+    lineHeight: 44,
+  },
+  secondText: {
+    color: "#939393",
+    lineHeight: 20,
   },
 });
