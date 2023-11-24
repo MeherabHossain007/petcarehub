3d5985d7d07a8e575779bc428eb15fa7885c1a0f
Yousufjoy <kanekiken333@gmail.com>
2023-11-24 12:27:49 +0600

    working on home page

--- frontend/screens/Home.tsx
+++ frontend/screens/Home.tsx
@@ -1,11 +1,43 @@
 import { Text } from "native-base";
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home!</Text>
-    </View>
+    <>
+      <View style={style.mainContainer}>
+        <View>
+          <Text style={style.text1}>All Your Pet Needs</Text>
+          <Text style={style.text2}>A Little </Text>
+          <Text style={style.text2}>Care</Text>
+          <Text style={style.text3}>From You</Text>
+        </View>
+      </View>
+    </>
   );
 }
+
+const style = StyleSheet.create({
+  mainContainer: {
+    // backgroundColor: "gray",
+
+    height: 250,
+    // borderWidth: 2,
+    // borderColor: "black",
+    // borderRadius: 10,
+    paddingLeft: 30,
+    marginTop: 100,
+    marginLeft: 70,
+  },
+  text1: {
+    fontSize: 16,
+  },
+  text2: {
+    fontSize: 20,
+    paddingLeft: 30,
+    color: "#2196F3",
+  },
+  text3: {
+    paddingLeft: 30,
+  },
+});
