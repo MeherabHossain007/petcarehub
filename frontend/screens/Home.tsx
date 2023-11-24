a4b27c24298265a8e1cfe053967a7a9fdd28bcab
Yousufjoy <kanekiken333@gmail.com>
2023-11-24 12:51:18 +0600

    homepage update

--- frontend/screens/Home.tsx
+++ frontend/screens/Home.tsx
@@ -1,11 +1,11 @@
-import { Text } from "native-base";
+import { Button, Text } from "native-base";
 import React from "react";
-import { View, StyleSheet } from "react-native";
-
+import { Image, View, StyleSheet } from "react-native";
+import logo from "../assets/logo.png";
 export default function Home() {
   return (
     <>
-      <View style={style.mainContainer}>
+      <View style={style.firstContainer}>
         <View>
           <Text style={style.text1}>All Your Pet Needs</Text>
           <Text style={style.text2}>A Little </Text>
@@ -13,15 +13,22 @@ export default function Home() {
           <Text style={style.text3}>From You</Text>
         </View>
       </View>
+      <View style={style.secondContainer}>
+        <Image style={style.logoStyle} source={logo} />
+        <Text style={style.logoText}>
+          Furry Friends, Forever Love. Unleash happiness at our pet shop, where
+          every pawprint finds its forever home
+        </Text>
+      </View>
     </>
   );
 }
 
 const style = StyleSheet.create({
-  mainContainer: {
+  firstContainer: {
     // backgroundColor: "gray",
 
-    height: 250,
+    height: 150,
     // borderWidth: 2,
     // borderColor: "black",
     // borderRadius: 10,
@@ -30,14 +37,37 @@ const style = StyleSheet.create({
     marginLeft: 70,
   },
   text1: {
-    fontSize: 16,
+    fontSize: 22,
+    lineHeight: 44,
   },
   text2: {
     fontSize: 20,
-    paddingLeft: 30,
+    paddingLeft: 50,
     color: "#2196F3",
+    lineHeight: 24,
+    fontWeight: "bold",
   },
   text3: {
-    paddingLeft: 30,
+    paddingLeft: 50,
+    lineHeight: 34,
+  },
+  secondContainer: {
+    height: 400,
+    position: "relative",
+    marginTop: 70,
+    backgroundColor: "#EEE9F2",
+  },
+  logoStyle: {
+    position: "absolute",
+    top: -80,
+    right: 100,
+  },
+  logoText: {
+    width: 300,
+    top: 100,
+    textAlign: "center",
+    right: -30,
+    color: "#939393",
+    marginTop: 10,
   },
 });
