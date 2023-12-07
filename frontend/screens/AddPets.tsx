fce28dccf084a5ff6ca56540985c0f8045a8c637
Yousufjoy <kanekiken333@gmail.com>
2023-12-07 20:07:05 +0600

    added photo option in the form

--- frontend/screens/AddPets.tsx
+++ frontend/screens/AddPets.tsx
@@ -1,3 +1,4 @@
+import { ScrollView } from "native-base";
 import React, { useState } from "react";
 import {
   View,
@@ -5,7 +6,9 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
+  Image,
 } from "react-native";
+import ImagePicker from "react-native-image-picker";
 
 const AddPets = () => {
   const [petName, setPetName] = useState("");
@@ -14,6 +17,18 @@ const AddPets = () => {
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [userLocation, setUserLocation] = useState("");
+  const [photo, setPhoto] = useState(null);
+
+  const handleChoosePhoto = () => {
+    const options = {
+      noData: true,
+    };
+    ImagePicker.launchImageLibrary(options, (response) => {
+      if (response.uri) {
+        setPhoto(response);
+      }
+    });
+  };
 
   const handleSubmission = () => {
     // Form submission Logic!
@@ -21,67 +36,81 @@ const AddPets = () => {
 
   return (
     <>
-      <View style={styles.container}>
-        <Text style={styles.label}>Pet Name:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter pet name"
-          value={petName}
-          onChangeText={(text) => setPetName(text)}
-        />
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.label}>Pet Name:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter pet name"
+            value={petName}
+            onChangeText={(text) => setPetName(text)}
+          />
 
-        <Text style={styles.label}>Pet Breed:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter pet breed"
-          value={petBreed}
-          onChangeText={(text) => setPetBreed(text)}
-        />
+          <Text style={styles.label}>Pet Breed:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter pet breed"
+            value={petBreed}
+            onChangeText={(text) => setPetBreed(text)}
+          />
 
-        <Text style={styles.label}>Pet Location:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter pet location"
-          value={petLocation}
-          onChangeText={(text) => setPetLocation(text)}
-        />
+          <Text style={styles.label}>Pet Location:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter pet location"
+            value={petLocation}
+            onChangeText={(text) => setPetLocation(text)}
+          />
 
-        <Text style={styles.label}>Email:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          keyboardType="email-address"
-        />
+          <Text style={styles.label}>Email:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            keyboardType="email-address"
+          />
 
-        <Text style={styles.label}>Phone Number:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your phone number"
-          value={phoneNumber}
-          onChangeText={(text) => setPhoneNumber(text)}
-          keyboardType="phone-pad"
-        />
+          <Text style={styles.label}>Phone Number:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your phone number"
+            value={phoneNumber}
+            onChangeText={(text) => setPhoneNumber(text)}
+            keyboardType="phone-pad"
+          />
 
-        <Text style={styles.label}>Your Location:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your location"
-          value={userLocation}
-          onChangeText={(text) => setUserLocation(text)}
-        />
+          <Text style={styles.label}>Your Location:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your location"
+            value={userLocation}
+            onChangeText={(text) => setUserLocation(text)}
+          />
 
-        <TouchableOpacity
-          style={styles.submitButton}
-          onPress={handleSubmission}
-        >
-          <Text style={styles.submitButtonText}>Submit</Text>
-        </TouchableOpacity>
-      </View>
+          <Text style={styles.label}>Pet Photo:</Text>
+          <TouchableOpacity onPress={handleChoosePhoto}>
+            <Text style={styles.uploadButtonText}>Upload Photo</Text>
+          </TouchableOpacity>
+          {photo && (
+            <Image
+              source={{ uri: photo.uri }}
+              style={{ width: 300, height: 300 }}
+            />
+          )}
+
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={handleSubmission}
+          >
+            <Text style={styles.submitButtonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     padding: 20,
@@ -99,6 +128,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   submitButton: {
+    marginTop: 20,
     padding: 10,
     alignItems: "center",
     borderRadius: 5,
@@ -106,6 +136,10 @@ const styles = StyleSheet.create({
   },
   submitButtonText: {
     color: "white",
+    fontSize: 16,
+  },
+  uploadButtonText: {
+    color: "#2196F3",
     fontSize: 16,
   },
 });
