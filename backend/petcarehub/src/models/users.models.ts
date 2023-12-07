a4660cd446532c371f4a76ab0f03b9c79f4f4769
Meherab Hossain <58097221+MeherabHossain007@users.noreply.github.com>
2023-12-07 08:18:46 +0600

    Add login and register endpoints to UsersController and implement corresponding methods in UsersService

---
+++ backend/petcarehub/src/models/users.models.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as readline from 'readline';
+
+@Entity('users')
+export class Users {
+  @PrimaryGeneratedColumn('increment', { name: 'id' })
+  readonly id?: number;
+
+  @Column('varchar', { length: 255, name: 'name', nullable: false })
+  readonly name: string;
+
+  @Column('varchar', { length: 255, name: 'email', nullable: false })
+  readonly email: string;
+
+  @Column('varchar', { length: 255, name: 'password', nullable: false })
+  readonly password: string;
+}
--- backend/petcarehub/src/users/users.service.ts
+++ backend/petcarehub/src/users/users.service.ts
@@ -1,4 +1,63 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDTO, UserCredentialDTO } from 'src/dto/users.dto';
+import { Users } from 'src/models/users.models';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(Users) private readonly userRepository: Repository<Users>,
+  ) {}
+
+  async getUser(email: string, password: string): Promise<Users> {
+    try {
+      const user = await this.userRepository.findOne({
+        where: {
+          email: email,
+          password: password,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      throw error;
+    }
+  }
+
+  async userAuth(userCredential: UserCredentialDTO): Promise<boolean> {
+    try {
+      const user = await this.getUser(
+        userCredential.email,
+        userCredential.password,
+      );
+      if (user && user.name) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      throw error;
+    }
+  }
+
+  async createUser(createUserDTO: CreateUserDTO): Promise<boolean> {
+    try {
+      console.log(createUserDTO.name);
+      console.log(createUserDTO.email);
+      console.log(createUserDTO.password);
+      const users = this.userRepository.create(createUserDTO);
+      const result = this.userRepository.save(users);
+
+      if (result) {
+        return true; // Success indication
+      } else {
+        return false; // Failure indication
+      }
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return false; // Return a failure indication in case of an error
+    }
+  }
+}
