47ae17d9afc768a86814bd46d27ea0a9388b3f8e
Meherab Hossain <58097221+MeherabHossain007@users.noreply.github.com>
2023-12-07 13:05:58 +0600

    Add database connection configuration

--- backend/petcarehub/src/app.module.ts
+++ backend/petcarehub/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import DatabaseConnection from './database/connection';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: DatabaseConnection,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
