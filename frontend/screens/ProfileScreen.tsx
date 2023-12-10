df920c99b359c404e9d2e9503f831ac3c0b020f4
Sadab-7878 <sadabuzzaman75@gmail.com>
2023-12-10 13:49:20 +0600

    profile added half

--- frontend/screens/ProfileScreen.tsx
+++ frontend/screens/ProfileScreen.tsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { TextInput } from "react-native-gesture-handler";
 
 const ProfileScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleUpdateProfile = () => {
+    console.log("Profile updated:", { name, email });
+  };
+
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      <Text style={styles.label}>Name:</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+
+      <Text style={styles.label}>Email:</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        keyboardType="email-address"
+      />
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
